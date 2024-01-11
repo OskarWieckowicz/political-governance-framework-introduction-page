a433ddccb745bf01fb98ecb37f9a75f40444de68
owieckowicz <owieckowicz@trans.eu>
2024-01-11 22:35:28 +0100

    conclusions

--- app/components/Info.tsx
+++ app/components/Info.tsx
@@ -6,6 +6,9 @@ const Info = () => {
   return (
     <Container>
       <Box sx={{ bgcolor: "background.paper", padding: { xs: 2, md: 10 } }}>
+        <Typography variant="h4" gutterBottom>
+          Info
+        </Typography>
         <Typography gutterBottom>
           This page presents a project I created. The goal was to create a
           project for the portfolio and at the same time learn some new
@@ -37,6 +40,78 @@ const Info = () => {
             LinkedIn
           </Link>
         </Button>
+        <Typography variant="h4" gutterBottom>
+          Conclusions
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
+          The subject matter was quite complicated and required a deep analysis
+          of political and economic aspects, which took a significant amount of
+          time. However, if I were to create a common topic, such as an app for
+          ordering food, I wouldn't have as much motivation to create it. The
+          ideological nature of the project was important to me. Paying taxes
+          with cryptocurrency is not currently practiced in any country, which
+          meant I had to carefully consider more aspects than the common
+          solutions I could follow. Moreover there is no point in looking at the
+          project through the prism of the applicable law in Poland, because it
+          certainly does not allow for the introduction of a similar tax payment
+          system.
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
+          After completing this project, I noticed a few key takeaways.
+          Regularity turned out to be key when working on such projects. The
+          more systematically you work on a project, the better. There were
+          times when I didn't make progress for a month, which resulted in a
+          loss of momentum and difficulty returning to work.
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
+          Another conclusion is that it is worth sticking to simplicity at the
+          beginning of a project. At the beginning, I approached the topic in
+          such a way that I wanted to use specific technologies and forcefully
+          find applications for them, which turned out to be disastrous when
+          planning the project. This was in contrast to the approach used in
+          commercial projects, where the priority is that everything works as
+          required and is done in the simplest way possible. It is better to
+          start with an MVP and gradually develop the application. Ultimately, I
+          gave up on many technologies that complicated the project and did not
+          add significant value. Nevertheless, I wanted to learn a few
+          technologies in practice, so they were included in the project.
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
+          When it comes to technologies, I gained a lot of experience, including
+          learning about Next.js - an extension of React with server side
+          rendering. I like imposing a project structure on pure React. However,
+          server-side rendering itself does add some complexity to development,
+          especially in a prototype application where the performance benefits
+          may not be as important. In addition, I also learned about the library
+          of ready-made components - Material UI.
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
+          Another technology learned is foundry - a tool for developing smart
+          contracts. I appreciated the ability to write tests in Solidity
+          instead of JavaScript, which set this approach apart from the
+          competition.
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
+          Generally speaking, building the frontend, backend and smart contract
+          did not cause me any major problems. I encountered the most
+          difficulties with Keycloak customization and deployment of the entire
+          project on AWS.
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
+          Keycloak is an open source authorization server that comes out with a
+          lot of ready-to-use functionality like admin panel, login and
+          registration forms, etc. It is a very powerful tool and its
+          configuration was quite hard because the documentation is poor. I
+          think it is not worth using for small projects, but has potential in
+          larger organizations with many applications.
+        </Typography>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
+          When it comes to AWS, I spent a lot of time deploying the application.
+          It's a powerful tool, but it requires setting up a lot of parameters
+          and permissions, which can be time-consuming. This type of project
+          will not use the full potential of AWS. It is a fantastic solution but
+          quite expensive.
+        </Typography>
       </Box>
     </Container>
   );
--- app/components/TechnicalArcticle.tsx
+++ app/components/TechnicalArcticle.tsx
@@ -30,7 +30,7 @@ const TechnicalArcticle = () => {
         <Typography variant="h4" gutterBottom>
           Technologies used
         </Typography>
-        <Typography variant="body2" sx={{ marginBottom: "20px" }}>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
           Frontend: Next.js, Material-UI <br />
           Backend: Spring Boot <br />
           Blockchain: Ethereum smart contract developed and tested using Foundry
@@ -43,7 +43,7 @@ const TechnicalArcticle = () => {
         <Typography variant="h4" gutterBottom>
           Repositories
         </Typography>
-        <Typography variant="body2" sx={{ marginBottom: "20px" }}>
+        <Typography variant="bobody1dy2" sx={{ marginBottom: "20px" }}>
           Frontend:{" "}
           <Typography
             component="a"
@@ -78,7 +78,7 @@ const TechnicalArcticle = () => {
         <Typography variant="h4" gutterBottom>
           Smart Contract
         </Typography>
-        <Typography variant="body2" sx={{ marginBottom: "20px" }}>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
           Smart Contract was written in Solidity programming language and was
           developed using Foundry - a smart contract development toolchain.
           Foundry manages your dependencies, compiles your project, runs tests,
@@ -95,7 +95,7 @@ const TechnicalArcticle = () => {
         <Typography variant="h4" gutterBottom>
           Frontend
         </Typography>
-        <Typography variant="body2" sx={{ marginBottom: "20px" }}>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
           Frontend was built using next js 13 and material UI for styling.
           Authentication mechanisms rely on NextAuth and Keycloak provider. For
           chart visualization rechart. Interaction with the smart contract
@@ -105,7 +105,7 @@ const TechnicalArcticle = () => {
         <Typography variant="h4" gutterBottom>
           Backend
         </Typography>
-        <Typography variant="body2" sx={{ marginBottom: "20px" }}>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
           The backend functions as a Spring Boot application structured into two
           Maven modules: the &apos;core&apos; module encompassing fundamental
           functionalities and the &apos;spi&apos; module housing the Keycloak
@@ -116,7 +116,7 @@ const TechnicalArcticle = () => {
           with the application&apos;s resources.
         </Typography>
 
-        <Typography variant="body2" sx={{ marginBottom: "20px" }}>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
           The application utilizes two distinct databases for different
           purposes: one dedicated solely to Keycloak, housing Keycloak&apos;s
           tables and user-related data, and another dedicated to the
@@ -126,7 +126,7 @@ const TechnicalArcticle = () => {
           database.
         </Typography>
 
-        <Typography variant="body2" sx={{ marginBottom: "20px" }}>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
           Communication with the blockchain is facilitated through the Web3J
           library, which provides a CLI tool enabling the generation of a Smart
           Contract Java wrapper class. Once this class is generated, a listener
@@ -137,14 +137,14 @@ const TechnicalArcticle = () => {
           application&apos;s blockchain interactions.
         </Typography>
 
-        <Typography variant="body2" sx={{ marginBottom: "20px" }}>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
           For real-time ETH/USD price updates, our application leverages a
           scheduler that periodically fetches this essential data from the Coin
           API. To comply with API request limits, the database undergoes updates
           every 15 minutes.
         </Typography>
 
-        <Typography variant="body2" sx={{ marginBottom: "20px" }}>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
           Furthermore, the Spring Boot application seamlessly integrates with
           AWS S3 for efficient and secure storage of user documents. To maintain
           code integrity and reliability, the test environment was configured so
@@ -155,7 +155,7 @@ const TechnicalArcticle = () => {
         <Typography variant="h4" gutterBottom>
           Keycloak
         </Typography>
-        <Typography variant="body2" sx={{ marginBottom: "20px" }}>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
           Keycloak undergoes customization through themes and SPIs. A custom
           registration form theme and two SPIs are implemented. The first SPI
           validates custom attributes in the registration form, while the second
@@ -165,7 +165,7 @@ const TechnicalArcticle = () => {
         <Typography variant="h4" gutterBottom>
           Local development
         </Typography>
-        <Typography variant="body2" sx={{ marginBottom: "20px" }}>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
           For local development Docker Compose is used to run databases and
           keycloak. It copies themes, SPIs and imports realms to the keycloak.
         </Typography>
@@ -173,14 +173,14 @@ const TechnicalArcticle = () => {
         <Typography variant="h4" gutterBottom>
           Deployment to AWS
         </Typography>
-        <Typography variant="body2" sx={{ marginBottom: "20px" }}>
+        <Typography variant="body1" sx={{ marginBottom: "20px" }}>
           The deployment process is automated by using github actions. It
           deploys spring boot and keycloak apps to AWS ECS on a new tag. At
           first Farget was used but it is not part of a AWS Free Tier and the
           deployment transitioned to EC2. NAT Gateway facilitates communication
           between the Spring Boot service, Keycloak, and the Coin API.
         </Typography>
-        <Typography variant="body2">
+        <Typography variant="body1">
           The frontend application operates on AWS Amplify and undergoes
           redeployment with every push to the main branch. Databases are hosted
           on RDS. Services operate behind load balancers, utilizing a custom
